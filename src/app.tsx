359099bdbef9078ba23b05d2dac4c41463f2468c
tomsal25 <113028016+tomsal25@users.noreply.github.com>
2023-05-09 05:34:12 +0900

    made header and split some components

--- src/app.tsx
+++ src/app.tsx
@@ -1,164 +1,157 @@
 import { css } from "@emotion/react";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import MuiAccordionSummary, {
-  AccordionSummaryProps,
-} from "@mui/material/AccordionSummary";
+import { PaletteMode } from "@mui/material";
 import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
-import { styled } from "@mui/material/styles";
-import React, { useState } from "react";
+import Toolbar from "@mui/material/Toolbar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+} from "./components/Accordion";
+import { Header } from "./components/Header";
+import { ColorModeContext } from "./stores/ColorModeContext";
 import { API_DATA, getApiData } from "./utils/getApiData";
 
-const Card = styled("div")`
-  width: 90vw;
-  height: 90vh;
-  background: skyblue;
-  overflow-y: scroll;
-`;
-
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  "&:not(:last-child)": {
-    borderBottom: 0,
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
   },
-  "&:before": {
-    display: "none",
-  },
-  "& pre": {
-    background: "lightgray",
-    marginLeft: "1em",
-    whiteSpace: "pre",
-  },
-}));
+});
 
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
-    {...props}
-  />
-))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === "dark"
-      ? "rgba(255, 255, 255, .05)"
-      : "rgba(0, 0, 0, .03)",
-  flexDirection: "row-reverse",
-  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-    transform: "rotate(90deg)",
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
   },
-  "& .MuiAccordionSummary-content": {
-    marginLeft: theme.spacing(1),
-  },
-}));
+});
 
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-}));
+export const App = () => {
+  const [theme, setTheme] = useState<PaletteMode>("light");
 
-export function App() {
-  const [count, setCount] = useState(1);
-  const [data, setData] = useState<readonly API_DATA[]>([]);
-  const [connect, setConnect] = useState(false);
-  const [codeExpanded, setCodeExpanded] = useState<string | false>(false);
-  const [testExpanded, setTestExpanded] = useState(false);
-
-  const handleChange =
-    (panel: string) => (_: React.SyntheticEvent, newExpanded: boolean) => {
-      setCodeExpanded(newExpanded ? panel : false);
-      setTestExpanded(!newExpanded && false);
-    };
+  const colorModeChanger = {
+    toggleColorMode: () => {
+      setTheme(prevMode => (prevMode == "light" ? "dark" : "light"));
+    },
+  };
 
   return (
     <>
-      <GlobalStyles
-        styles={{
-          "*": {
-            padding: "0",
-            margin: "0",
-          },
-          body: {
-            width: "100vw",
-            height: "100vh",
-          },
-          "body, #app": {
-            display: "flex",
-            placeItems: "center",
-            justifyContent: "center",
-          },
-        }}
-      />
-      <Card>
-        <Button
-          variant="contained"
-          disabled={connect}
-          onClick={() => {
-            setConnect(true);
-            getApiData(count)
-              .then(dt => {
-                setData([...data, ...dt]);
-                setCount(count => count + 1);
-                // console.table(dt);
-              })
-              .catch(err => {
-                if (import.meta.env.DEV) {
-                  console.log(err);
-                }
-              })
-              .finally(() => {
-                setConnect(false);
-              });
-          }}
-        >
-          {count}
-        </Button>
-
-        <div>
-          {data.map((dt, i) => (
-            <Accordion
-              expanded={codeExpanded === `panel${i}`}
-              onChange={handleChange(`panel${i}`)}
-              key={i}
-              css={css`
-                margin-bottom: 2em;
-              `}
-            >
-              <AccordionSummary>
-                {dt.id}: {dt.description}
-              </AccordionSummary>
-              <AccordionDetails>
-                <pre
-                  css={css`
-                    margin-bottom: 1em;
-                  `}
-                >
-                  <code>{dt.code}</code>
-                </pre>
-                <Accordion
-                  expanded={testExpanded}
-                  onChange={(_, expanded) =>
-                    setTestExpanded(!!codeExpanded && expanded)
-                  }
-                >
-                  <AccordionSummary>test</AccordionSummary>
-                  <AccordionDetails
-                    css={css`
-                      margin: 1em;
-                    `}
-                  >
-                    <pre>
-                      <code>{dt.test}</code>
-                    </pre>
-                  </AccordionDetails>
-                </Accordion>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </div>
-      </Card>
+      <ColorModeContext.Provider value={colorModeChanger}>
+        <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
+          <CssBaseline />
+          <GlobalStyles
+            styles={{
+              "*": {
+                padding: "0",
+                margin: "0",
+              },
+            }}
+          />
+          <Header />
+          {/* for margin */}
+          <Toolbar />
+          <DataItemList />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </>
+  );
+};
+
+function DataItemList() {
+  const [count, setCount] = useState(1);
+  const [data, setData] = useState<readonly API_DATA[]>([]);
+  const [connect, setConnect] = useState(false);
+
+  return (
+    <Container sx={{ my: 2 }}>
+      <Button
+        variant="contained"
+        disabled={connect}
+        onClick={() => {
+          setConnect(true);
+          getApiData(count)
+            .then(dt => {
+              setData([...data, ...dt]);
+              setCount(count => count + 1);
+              // console.table(dt);
+            })
+            .catch(err => {
+              if (import.meta.env.DEV) {
+                console.log(err);
+              }
+            })
+            .finally(() => {
+              setConnect(false);
+            });
+        }}
+      >
+        {count}
+      </Button>
+
+      <>
+        {data.map((dt, i) => (
+          <DataItem key={i} dt={dt} />
+        ))}
+      </>
+    </Container>
+  );
+}
+
+function DataItem({ dt }: { dt: API_DATA }) {
+  const [codeExpanded, setCodeExpanded] = useState<boolean>(false);
+
+  const handleExpand = () => {
+    setCodeExpanded(!codeExpanded);
+  };
+
+  return (
+    <Accordion expanded={codeExpanded} onChange={handleExpand}>
+      <AccordionSummary>
+        {dt.id}: {dt.description}
+      </AccordionSummary>
+      <AccordionDetails>
+        <pre
+          css={css`
+            margin-bottom: 1em;
+          `}
+        >
+          <code>{dt.code}</code>
+        </pre>
+        <NewFunction_1 codeExpanded={codeExpanded} dt={dt} />
+      </AccordionDetails>
+    </Accordion>
+  );
+}
+
+function NewFunction_1({
+  codeExpanded,
+  dt,
+}: {
+  codeExpanded: boolean;
+  dt: API_DATA;
+}) {
+  const [testExpanded, setTestExpanded] = useState(false);
+
+  useEffect(() => {
+    if (!codeExpanded) {
+      setTestExpanded(false);
+    }
+  }, [codeExpanded]);
+
+  return (
+    <Accordion
+      expanded={testExpanded}
+      onChange={() => setTestExpanded(codeExpanded && !testExpanded)}
+    >
+      <AccordionSummary>test</AccordionSummary>
+      <AccordionDetails>
+        <pre>
+          <code>{dt.test}</code>
+        </pre>
+      </AccordionDetails>
+    </Accordion>
   );
 }
