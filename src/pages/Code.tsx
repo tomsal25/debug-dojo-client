dd60b3e20ea8177ac54c33c8d72e78ce6bf008f6
tomsal25 <113028016+tomsal25@users.noreply.github.com>
2023-06-07 07:32:34 +0900

    made check test code

--- src/pages/Code.tsx
+++ src/pages/Code.tsx
@@ -6,6 +6,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import ReplayIcon from "@mui/icons-material/Replay";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Fab from "@mui/material/Fab";
 import Fade from "@mui/material/Fade";
@@ -13,6 +14,7 @@ import Modal from "@mui/material/Modal";
 import Switch from "@mui/material/Switch";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import { SxProps, Theme } from "@mui/material/styles";
 import { editor } from "monaco-editor";
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -47,6 +49,42 @@ const CodeInfo = ({ id, info }: { id: number; info: string }) => {
   );
 };
 
+const modalStyle: SxProps<Theme> = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%,-50%)",
+  width: 600,
+  maxWidth: "95vw",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 3,
+};
+
+const CodeDisplayModal = ({
+  code,
+  disabled,
+}: {
+  code: string;
+  disabled?: boolean;
+}) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button disabled={disabled} onClick={() => setOpen(true)}>
+        Check Test Code
+      </Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box sx={{ ...modalStyle, width: 400 }}>
+          <Editor height="40vh" options={{ readOnly: true }} value={code} />
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
 interface ResultInit {
   type: 0;
 }
@@ -58,6 +96,7 @@ interface ResultTesting {
 interface ResultAnswer {
   type: 2;
   pass: boolean;
+  test: string;
 }
 
 interface ResultError {
@@ -103,33 +142,32 @@ const ResultModal = ({
     return <Typography sx={{ visibility: "hidden" }}>init</Typography>;
   };
 
+  const TestCode = () => {
+    if (result.type != 2) return <CodeDisplayModal code="" disabled />;
+
+    return <CodeDisplayModal code={result.test} />;
+  };
+
   return (
     <Modal open={open}>
       <Fade in={open}>
         <Box
           sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%,-50%)",
-            width: 600,
-            maxWidth: "95vw",
-            bgcolor: "background.paper",
-            border: "2px solid #000",
-            boxShadow: 24,
-            p: 3,
+            ...modalStyle,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
           }}
         >
-          <Box sx={{ mb: 2 }}>
-            <Typography variant="h6" component="h2">
-              Result
-            </Typography>
-          </Box>
-
-          <Box sx={{ mb: 2, textAlign: "center" }}>
+          {/* title */}
+          <Typography variant="h6" component="h2">
+            Result
+          </Typography>
+          {/* result */}
+          <Box sx={{ textAlign: "center" }}>
             <ResultText />
           </Box>
-
+          {/* select button */}
           <Box
             sx={{
               display: "flex",
@@ -158,6 +196,10 @@ const ResultModal = ({
               disabled={isTesting}
               href={HrefList.random}
             />
+          </Box>
+          {/* show test code */}
+          <Box sx={{ textAlign: "left" }}>
+            <TestCode />
           </Box>
         </Box>
       </Fade>
@@ -189,6 +231,7 @@ const Code = ({ rawData }: { rawData: API_DATA }) => {
           setResult({
             type: 2,
             pass: res.every(flag => flag.type == "code" && flag.flag),
+            test: rawData.test,
           });
         }, displayDelay);
       })
